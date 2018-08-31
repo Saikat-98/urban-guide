df0e124d8a2f2d9ac93b14207a17dad6bf1460f3
Saikat-98 <30549072+Saikat-98@users.noreply.github.com>
2018-08-31 22:18:31 +0530

    Reevaluated

--- assignment2/LeapYear.java
+++ assignment2/LeapYear.java
@@ -6,9 +6,9 @@ public class LeapYear
 		Scanner reader=new Scanner(System.in);
 		System.out.println("Enter the year:");
 		int x=reader.nextInt();
-		if(x%4==0)
+		if(x%400==0&&x%100==0&&x%400==0)
 		System.out.println("Leap year!");
 		else
-		System.out.println("Not a leap year!");
+		System.out.println("Not a Leap year!");
 	}
 }
